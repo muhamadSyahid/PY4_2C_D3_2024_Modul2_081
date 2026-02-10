0e9ea071e1118c8d8f409ddcb42d22cfad1cc560
muhamadSyahid <muhamadsyahid2016@gmail.com>
2026-02-10 10:01:48 +0700

    Initiate Task 1

---
+++ lib/counter_controller.dart
@@ -0,0 +1,38 @@
+class CounterController {
+  int _counter = 10;
+  int _step;
+  final List<String> _history = [];
+
+  CounterController({int initialStep = 1}) : _step = initialStep;
+  int get value => _counter;
+  int get currentStep => _step;
+  List<String> get currentHistory => _history;
+
+  void incrementCounter() {
+    int before = _counter;
+    _counter += _step;
+    _addLog("Menambahkan", before, _counter);
+  }
+
+  void decrementCounter() {
+    int before = _counter;
+    if (_counter > 0) _counter -= _step;
+    _addLog("Mengurangi", before, _counter);
+  }
+
+  void resetCounter() {
+    int before = _counter;
+    _counter = 0;
+    _addLog("Mengurangi", before, _counter);
+  }
+
+  void setStep(int newStep) {
+    _step = newStep;
+  }
+
+  void _addLog(String action, int from, int to) {
+    String time =
+        "${DateTime.now().hour}:${DateTime.now().minute.toString().padLeft(2, '0')}";
+    _history.insert(0, "User $action dari $from ke $to di jam $time");
+  }
+}
---
+++ lib/counter_view.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'counter_controller.dart';
+
+class CounterView extends StatefulWidget {
+  const CounterView({super.key});
+
+  @override
+  State<CounterView> createState() => _CounterViewState();
+}
+
+class _CounterViewState extends State<CounterView> {
+  final CounterController _controller = CounterController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Logbook After SRP"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Total Hitungan',
+            ),
+            Text(
+              '${_controller.value}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () =>
+                        setState(() => _controller.decrementCounter()),
+                    icon: const Icon(Icons.arrow_back_rounded)),
+                IconButton(
+                    onPressed: () =>
+                        setState(() => _controller.incrementCounter()),
+                    icon: const Icon(Icons.arrow_forward_rounded)),
+                IconButton(
+                    onPressed: () => setState(() => _controller.resetCounter()),
+                    icon: const Icon(Icons.refresh_rounded)),
+              ],
+            ),
+            SizedBox(),
+            const Text(
+              "Total Step",
+            ),
+            Text(
+              '${_controller.currentStep}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Slider(
+                value: _controller.currentStep.toDouble(),
+                min: 0,
+                max: 10,
+                divisions: 9,
+                onChanged: (double newValue) {
+                  setState(() => _controller.setStep(newValue.toInt()));
+                }),
+            Text("5 Riwayat Terakhir"),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _controller.currentHistory.length > 5
+                    ? 5
+                    : _controller.currentHistory.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.history),
+                          title: Text(_controller.currentHistory[index]),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () => setState(() => _controller.incrementCounter()),
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+    );
+  }
+}
